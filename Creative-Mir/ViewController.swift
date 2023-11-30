a30c7f5c9db1eacac0e4593e82b0d4a961ca69c4
irinapechik <irinapechik@gmail.com>
2023-11-30 12:22:08 +0300

    Created project

---
+++ Creative-Mir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Creative-Mir
+//
+//  Created by Печик Дарья on 30.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
