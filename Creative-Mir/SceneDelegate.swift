d68ee5c685f77e1d0d4cb55a89ab0d99316ae5b7
irinapechik <irinapechik@gmail.com>
2023-12-16 19:45:43 +0300

    create MainPageView

--- Creative-Mir/SceneDelegate.swift
+++ Creative-Mir/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        let viewController = AutorizationPageViewController()
+        let viewController = MainPageViewController()
         window.rootViewController = UINavigationController(rootViewController: viewController)
         window.makeKeyAndVisible()
         self.window = window
---
+++ Creative-Mir/Views/MainPageView.swift
@@ -0,0 +1,24 @@
+//
+//  MainPageView.swift
+//  Creative-Mir
+//
+//  Created by Печик Ирина on 16.12.2023.
+//
+
+import Foundation
+import UIKit
+
+class MainPageView: UIView {
+    
+    // MARK: - Fields
+    // MARK: - Initialization
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private methods
+}
